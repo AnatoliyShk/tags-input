04aaa0b6b3838536a423d5b235c8f45eba40f49a
Anatoliy Shekera <tolikshekera@gmail.com>
2026-02-11 03:42:49 +0200

    Initital commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import TagsInput from './components/TagsInput.vue';
+
+  export default {
+    components: {
+      TagsInput
+    }
+  };
+</script>
+
+<template>
+  <h1>Hello World</h1>
+  <TagsInput />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/TagsInput.vue
@@ -0,0 +1,13 @@
+<template>
+    <div v-for="(tag, index) in tags"> {{ index + " : " + tag }} </div>
+    <input type="text" :value="newTag" v-on:keydown.enter="tags.push($event.target.value)" />
+</template>
+
+<script>
+export default {
+    data: () => ({
+        tags: ["vue", "react", "angular"],
+        newTag: "test"
+    }),
+}
+</script>
