c12f4bb118ef2837e1774198a6d9f93cadcac06a
Anatoliy Shekera <tolikshekera@gmail.com>
2026-02-12 22:26:24 +0200

    Tags input changes

--- src/components/TagsInput.vue
+++ src/components/TagsInput.vue
@@ -1,12 +1,17 @@
 <template>
-    <div v-for="(tag, index) in tags"> {{ index + " : " + tag }} </div>
+    <div v-for="(tag, index) in tags">
+        {{ tag }}
+         <button @click="removeTag(index)">x</button>
+    </div>
     <hr/>
     {{ newTag }}
     <input
         type="text"
         v-model.trim="newTag"
-        @keydown.enter="tags.push($event.target.value)"
-        @keydown.tab.prevent="tags.push($event.target.value)"
+        @keydown.enter="addNewTag"
+        @keydown.delete="removeLastTag"
+        @keydown.tab.prevent="addNewTag"
+        :class="tags.includes(newTag) ? 'tag-exists' : ''"
     />
 </template>
 
@@ -14,7 +19,30 @@
 export default {
     data: () => ({
         tags: ["vue", "react", "angular"],
-        newTag: "test"
+        newTag: ""
     }),
+    methods: {
+        addNewTag() {
+            if(this.newTag && !this.tags.includes(this.newTag)) {
+                this.tags.push(this.newTag);
+                this.newTag = "";
+            }
+        },
+        removeTag(index) {
+            this.tags.splice(index, 1);
+        },
+        removeLastTag() {
+            if(this.newTag.length === 0) {
+                this.removeTag(this.tags.length - 1);
+            }
+        }
+    }
 }
 </script>
+
+<style scoped>
+.tag-exists{
+    color: red;
+    text-decoration: line-through;
+}
+</style>
